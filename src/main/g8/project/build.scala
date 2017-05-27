37968e70ed9f454028417f175f7c912d34a40f60
Nando Sola <nandosola@users.noreply.github.com>
2017-05-27 11:00:04 +0200

    Update build.scala

--- src/main/g8/project/build.scala
+++ src/main/g8/project/build.scala
@@ -30,15 +30,15 @@ object $name;format="Camel"$Build extends Build {
       libraryDependencies ++= Seq(
         "org.scalatra" %% "scalatra" % ScalatraVersion,
         "org.scalatra" %% "scalatra-scalatest" % ScalatraVersion % "test",
-        "ch.qos.logback" % "logback-classic" % "1.1.2" % "runtime",
+        "ch.qos.logback" % "logback-classic" % "1.2.3" % "runtime",
         "org.scalatra" %% "scalatra-json" % ScalatraVersion,
-        "org.json4s"   %% "json4s-jackson" % "3.2.11",
+        "org.json4s"   %% "json4s-jackson" % "3.5.2",
         "com.typesafe.akka" %% "akka-actor" % akkaVersion,
-        "com.typesafe.akka" % "akka-slf4j_2.11" % akkaVersion,
-        "com.typesafe.akka" % "akka-testkit_2.11" % akkaVersion % "test",
-        "org.eclipse.jetty" % "jetty-webapp" % "9.1.5.v20140505" % "container",
-        "org.eclipse.jetty" % "jetty-plus" % "9.1.5.v20140505" % "container",
-        "javax.servlet" % "javax.servlet-api" % "3.1.0"
+        "com.typesafe.akka" %% "akka-slf4j" % akkaVersion,
+        "com.typesafe.akka" %% "akka-testkit" % akkaVersion % "test",
+        "org.eclipse.jetty" % "jetty-webapp" % "9.4.5.v20170502" % "container",
+        "org.eclipse.jetty" % "jetty-plus" % "9.4.5.v20170502" % "container",
+        "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided"
       )
     )
   )
